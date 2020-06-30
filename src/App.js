1b0deb0158a0bdfe0b7e914d62a824fe72e1dcb9
amir mahdi soleimani <mohdis.solimani@gmail.com>
2020-07-01 03:23:28 +0430

    routing added and components are extracted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import HomePage from './components/homepage.component'
+import {Route, Switch} from 'react-router-dom'
+import HomePage from './pages/homepage.component'
 import './App.css';
 
+const Hats = () => <div><h1>Hello Hats</h1></div>
+
 function App() {
-  return <HomePage / >
+
+  return(
+  <div>
+    <Switch>
+      <Route exact path = '/' component={HomePage}/>
+      <Route exact path = '/hats' component = {Hats} />
+    </Switch>
+  </div>
+  ) 
+
 }
 
 export default App;
