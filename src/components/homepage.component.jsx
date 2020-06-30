6067493d7add83329c3f8b0d42972ba328e98f6f
amir mahdi soleimani <mohdis.solimani@gmail.com>
2020-06-30 20:15:00 +0430

    homepage added and first initilize done

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './homepage.style.scss'
+
+const HomePage = () => {
+  return <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">Title</h2>
+          <span className="subtitle">subtitle</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">Title</h2>
+          <span className="subtitle">subtitle</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">Title</h2>
+          <span className="subtitle">subtitle</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">Title</h2>
+          <span className="subtitle">subtitle</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">Title</h2>
+          <span className="subtitle">subtitle</span>
+        </div>
+      </div>
+    </div>
+  </div>
+}
+export default HomePage
